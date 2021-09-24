8baacb9fc75962e099bec4d1289bad2bf5a5ab7b
bibekdhkl <bibekdh2016@gmail.com>
2021-09-24 23:47:53 +0545

    Jade and cookies

--- Day9/index.js
+++ Day9/index.js
@@ -4,20 +4,38 @@ const path = require('path');
 //this module provides absolute path
 
 // it returns the path of the folder in which it is located which is especially used during deployment
-console.log(__dirname);
+// console.log(__dirname);
+
+//to tell express we have used JADE
+//we have folder path via middleware
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","jade");
 
 app.use('/',(req,res) => {
       // res.sendFile('./public/hello.txt','test.txt')
+      //we can send html file via this too
       //Sends file names hello.txt in the form of test.txt to frontend
       //but it don't work since it needs absolute path
 
+      // res.sendFile(path.join(__dirname,"/public/file.json"));
       // res.sendFile(path.join(__dirname,"/public/hello.txt"),"text.txt");
       //After this the browser renders the file while we can send by not sending the file name test.txt
 
-      res.download(path.join(__dirname,"/public/file.json"),"downloadfile.json")
+      // res.download(path.join(__dirname,"/public/file.json"),"downloadfile.json")
       //downloads the file that is sent
 
-      
+      res.render('index',{title: "Assign_values_to_variables"})
+      //It assigns the values to variables inside jade files for dynamic files
+      //Unlike sendFile and download we need to specify the path of the file we are going to use
+
+      res
+      .status(201)
+      .cookie("test-cookie","test")
+      .cookie("test2","test",{
+            expire: new Date(Date.now() + 8 * 3600000)
+      })
+      .redirect(301,"/admin") 
+      //It has multiple pipelines
 })
 
 //listens at a port
--- Day9/readme.md
+++ Day9/readme.md
@@ -2,10 +2,19 @@
 
 * console.log(__dirname) => It returns the path of the directory in which it is located
 
-* sendFile() => sends files to frontend from server by automatically sending the content-type header file
+* sendFile() => sends files to frontend from server by automatically sending the content-type header file so that it can be rendered by the browser
 * download() => downloads the file that is sent
 - sendFile() and download() works in all types of static files
 
+* render() => When we have to work on dynamic files which is sent to the site by rendering into html files
+* _dirname => it returns the path of the folder in which it is located which is especially used during deployment
 * path.join() => we use path module to concat two paths like as in string concatenation
-* Headers in Network of inspect element is just like meta tag in HTML which provides information to the browser regarding the content
- -------------------------------    
+* Headers in Network of inspect element is just like meta tag in HTML which provides information to the browser regarding the content 
+ * jade is smaller type of HTML in which we have flexiblity in Variables like declaring and assigning.
+      so to install
+      ```
+      npm i -s jade
+      ```
+      And then We can work with jade files as requirements. Jade convers into html after inserting values to the variables so that it can be rendered by the browser. Also similar task where we insert value can be done via eJS. 
+
+* Cookies are used in frontend for authentication and authorization which also can be set via backend
